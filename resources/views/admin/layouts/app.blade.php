aefd744a384bf2a5c39da759effec0ab696c112c
Aisha-Moon <imtiajhanimoon@gmail.com>
2024-09-17 08:49:48 +0600

    changed title

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>{{ !empty($header_title) ? $header_title : '' }} -M.S</title>
+  <title>DriveNow </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
@@ -43,15 +43,13 @@
               <div class="d-flex justify-content-center py-4">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="{{ asset('assets/img/logo.png') }}" alt="">
-                  <span class="d-none d-lg-block">Property M.S</span>
+                  <span class="d-none d-lg-block">DriveNow</span>
                 </a>
               </div><!-- End Logo -->
 
               @yield('content')
 
-              <div class="credits">
-                Designed by <a href="https://bootstrapmade.com/">AishaMoon</a>
-              </div>
+             
 
             </div>
           </div>
--- resources/views/admin/layouts/app1.blade.php
+++ resources/views/admin/layouts/app1.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>{{ !empty($header_title) ? $header_title : '' }} - Property management system </title>
+  <title>DriveNow Admin Panel </title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
